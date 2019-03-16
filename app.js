e484d6664a295e766a22cbea9264f3fa8bd013ae
HP Spieler <sp@pcx.de>
2019-03-16 19:42:49 +0100

    Basic app.js erstellt

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// https://medium.freecodecamp.org/how-to-deploy-your-super-cool-node-app-to-azure-from-github-47ebff6c5448
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello, World!</h1>');
+});
+server.listen(port,() => {
+    console.log('Server running at port '+port);
+});
